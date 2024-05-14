7363c592ab422ad8cf9565727759a01aed810e5a
Eliza Cohen <elizacohen5@gmail.com>
2024-05-13 21:26:29 -0400

    complete Lists and Keys Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  const linkItems = links.map((link) => {
+      let href = `#${link}`;
+      return <a href={href} key={link}>{link}</a>
+    })
+
+  return (
+    <div>
+      <ol>
+        {linkItems}
+      </ol>
+    </div>
+  )
+
+  }
+
 
 export default NavBar;
